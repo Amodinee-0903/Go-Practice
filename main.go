e4d87ca23236829d86a6e538b73a46aaade72f35
Amodinee_0903 <amodinee.0903@gmail.com>
2026-03-09 20:10:13 +0530

    feat-loops

--- main.go
+++ main.go
@@ -35,4 +35,33 @@ func main() {
 	} else {
 		fmt.Println("Minor")
 	}
+	printloops()
+}
+
+/* Write a Go program that:1)Prints numbers 1 to 10 2)Prints only even numbers from 1 to 20
+3)Calculates sum of numbers from 1 to 5 */
+func printloops() {
+
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println() // newline
+
+	fmt.Println("Only Even Numbers:")
+
+	for i := 1; i <= 20; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%d ", i)
+		}
+	}
+
+	fmt.Println() // newline
+
+	sum := 0
+	for i := 1; i <= 5; i++ {
+		sum += i
+	}
+
+	fmt.Println("Sum is:", sum)
 }
